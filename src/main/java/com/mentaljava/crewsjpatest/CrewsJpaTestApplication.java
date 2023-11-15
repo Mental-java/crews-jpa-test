d5ad0de2a55dd148074b689f53198cc03e7bef19
Hongse0 <hongseyoung981007@gmail.com>
2023-11-15 20:21:46 +0900

    test

---
+++ src/main/java/com/mentaljava/crewsjpatest/CrewsJpaTestApplication.java
@@ -0,0 +1,13 @@
+package com.mentaljava.crewsjpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewsJpaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewsJpaTestApplication.class, args);
+    }
+
+}
