cadf5077cf74d857fd7d0dbce50dc4218d8e0752
DESKTOP-98JOUDC\user <jojoo9279@gmail.com>
2023-11-15 21:16:50 +0900

    [admin 도메인]

---
+++ src/main/java/com/mentaljava/crewsjpatest/domain/admin.java
@@ -0,0 +1,24 @@
+package com.mentaljava.crewsjpatest.domain;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name="admin")
+public class admin {
+
+    @Id
+    @Column(name="ADMIN_ID")
+    private String ADMIN_ID;
+
+    @Column(name="ADMIN_PW")
+    private String ADMIN_PW;
+
+    @Column(name="PERMSSION_NAME")
+    private String PERMISSION_NAME;
+}
