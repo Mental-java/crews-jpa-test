9328f2411b20e9f824273bddee581854d4ad5164
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-11-15 22:08:23 +0900

    컨트롤러 추가

---
+++ src/main/java/com/mentaljava/crewsjpatest/controller/UserCalendarController.java
@@ -0,0 +1,10 @@
+package com.mentaljava.crewsjpatest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Home")
+public class UserCalendarController {
+
+}
